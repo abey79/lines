Improved rounding precision, fixing new lego example. Many new tests.
Status: all tests pass, all examples render correctly!

lines/math.py
@@ -6,6 +6,9 @@ import numpy as np
 import shapely.ops
 from shapely.geometry import Polygon, asLineString
 
+RTOL = 1e-14
+ATOL = 1e-14
+
 
 def _validate_segments(segments: np.ndarray) -> None:
     if len(segments.shape) != 3 or segments.shape[1:] != (2, 3):
@@ -195,24 +198,40 @@ def triangles_overlap_segment_2d(
     t2t1 = -t1t2
     t0t2 = -t2t0
 
-    decimals = 14
-
-    f1 = (np.cross(t0t1, t0p0) * np.cross(t0t1, t0t2)).round(decimals)
-    f2 = (np.cross(t0t1, t0t2) * np.cross(t0t1, t0p1)).round(decimals)
-
-    f3 = (np.cross(t1t2, t1p0) * np.cross(t1t2, t1t0)).round(decimals)
-    f4 = (np.cross(t1t2, t1t0) * np.cross(t1t2, t1p1)).round(decimals)
-
-    f5 = (np.cross(t2t0, t2p0) * np.cross(t2t0, t2t1)).round(decimals)
-    f6 = (np.cross(t2t0, t2t1) * np.cross(t2t0, t2p1)).round(decimals)
-
     p0p1_cross_p0t0 = np.cross(p0p1, p0t0)
     p0p1_cross_p0t1 = np.cross(p0p1, p0t1)
     p0p1_cross_p0t2 = np.cross(p0p1, p0t2)
 
-    f7 = (p0p1_cross_p0t0 * p0p1_cross_p0t1).round(decimals)
-    f8 = (p0p1_cross_p0t1 * p0p1_cross_p0t2).round(decimals)
-    f9 = (p0p1_cross_p0t2 * p0p1_cross_p0t0).round(decimals)
+    f = np.stack(
+        (
+            np.cross(t0t1, t0p0) * np.cross(t0t1, t0t2),  # f1
+            np.cross(t0t1, t0t2) * np.cross(t0t1, t0p1),  # f2
+            np.cross(t1t2, t1p0) * np.cross(t1t2, t1t0),  # f3
+            np.cross(t1t2, t1t0) * np.cross(t1t2, t1p1),  # f4
+            np.cross(t2t0, t2p0) * np.cross(t2t0, t2t1),  # f5
+            np.cross(t2t0, t2t1) * np.cross(t2t0, t2p1),  # f6
+            p0p1_cross_p0t0 * p0p1_cross_p0t1,  # f7
+            p0p1_cross_p0t1 * p0p1_cross_p0t2,  # f8
+            p0p1_cross_p0t2 * p0p1_cross_p0t0,  # f9
+        ),
+        axis=0,
+    )
+
+    # Here we need to carefully compute the tolerance. All the f-numbers are products of 2
+    # cross-products, so in the order of the specific length to the power of 4.
+    f_atol = RTOL * (np.linalg.norm(p0p1) ** 4) * 1e2
+
+    f = np.where(np.isclose(f, 0, atol=f_atol), 0, f)
+
+    f1 = f[0]
+    f2 = f[1]
+    f3 = f[2]
+    f4 = f[3]
+    f5 = f[4]
+    f6 = f[5]
+    f7 = f[6]
+    f8 = f[7]
+    f9 = f[8]
 
     # /* If segment is strictly outside triangle, or triangle is strictly
     #  * apart from the line, we're not intersecting */
@@ -345,7 +364,7 @@ def segments_parallel_to_face(
     if p0.shape != (3,) or n.shape != (3,):
         raise ValueError(f"p0 and n must be of length 3")
 
-    if np.isclose(n[2], 0):
+    if np.isclose(n[2], 0, atol=ATOL, rtol=RTOL):
         raise ValueError(f"plane should not be parallel to z axis")
 
     # make sure n points up
@@ -360,7 +379,7 @@ def segments_parallel_to_face(
     output.fill(ParallelType.NOT_PARALLEL.value)
 
     # Test for parallelism: dot(sv, n) == 0
-    para_idx, = np.where(np.isclose(np.dot(sv, n), 0))
+    para_idx, = np.where(np.isclose(np.dot(sv, n), 0, atol=ATOL, rtol=RTOL))
 
     # dot(s0 - p0, n) is:
     # 0 for coincidence
@@ -368,7 +387,7 @@ def segments_parallel_to_face(
     # <0 if s0 is other side as n
 
     prod = np.dot(s0[para_idx] - p0, n)
-    idx1 = np.isclose(prod, 0)
+    idx1 = np.isclose(prod, 0, atol=ATOL, rtol=RTOL)
     idx2 = np.logical_and(~idx1, prod > 0)
     idx3 = np.logical_and(~idx1, prod < 0)
 
@@ -412,7 +431,7 @@ def segment_parallel_to_planes(
     output.fill(ParallelType.NOT_PARALLEL.value)
 
     # Test for parallelism: dot(sv, n) == 0
-    para_idx, = np.where(np.isclose(np.dot(n, sv), 0, atol=1e-16))
+    para_idx, = np.where(np.isclose(np.dot(n, sv), 0, atol=ATOL, rtol=RTOL))
 
     # dot(s0 - p0, n) is:
     # 0 for coincidence
@@ -422,7 +441,7 @@ def segment_parallel_to_planes(
     if len(para_idx) > 0:
         # prod = np.tensordot(s0 - p0[para_idx], n[para_idx], axes=(1, 1))
         prod = np.sum((s0 - p0[para_idx]) * n[para_idx], axis=1)
-        idx = np.isclose(prod, 0, atol=1e-16)
+        idx = np.isclose(prod, 0, atol=ATOL, rtol=RTOL)
         output[para_idx[idx]] = ParallelType.PARALLEL_COINCIDENT.value
         output[para_idx[np.logical_and(~idx, prod > 0)]] = ParallelType.PARALLEL_FRONT.value
         output[para_idx[np.logical_and(~idx, prod < 0)]] = ParallelType.PARALLEL_BACK.value
@@ -480,7 +499,7 @@ def mask_segments(segments: np.array, mask: np.array, diff: bool = True) -> np.a
         res = getattr(ls, op)(poly)
 
         # segments with identical start/stop location are sometime returned
-        if np.isclose(res.length, 0):
+        if np.isclose(res.length, 0, atol=ATOL, rtol=RTOL):
             continue
 
         if res.geom_type == "LineString":
@@ -610,14 +629,14 @@ def segment_triangle_intersection(segment, triangle):
     else:
         swap_st = False
 
-    #     if (n == (Vector)0)             // triangle is degenerate
-    #         return -1;                  // do not deal with this case
-    if np.linalg.norm(n) == 0:
-        return DEGENERATE, 0, 0, 0, (0, 0, 0), 0
-
     #     sv = R.P1 - R.P0;              // ray direction vector
     sv = segment[1] - segment[0]
     s0 = segment[0]
+
+    #     if (n == (Vector)0)             // triangle is degenerate
+    #         return -1;                  // do not deal with this case
+    if np.isclose(np.linalg.norm(n), 0, atol=ATOL):
+        return DEGENERATE, 0, 0, 0, (0, 0, 0), 0
 
     #     w0 = R.P0 - T.V0;
     w0 = s0 - triangle[0]
@@ -633,8 +652,9 @@ def segment_triangle_intersection(segment, triangle):
     #             return 2;
     #         else return 0;              // ray disjoint from plane
     #     }
-    if np.linalg.norm(b) < 1e-13:  # parallel testing could be integrated here
-        if np.isclose(a, 0, atol=1e-13):
+    atol = np.linalg.norm(sv) * RTOL
+    if np.isclose(np.linalg.norm(b), 0, atol=atol):
+        if np.isclose(a, 0, atol=atol):
             return INT_COPLANAR, None, None, None, None, 0
         elif a <= 0:
             return NO_INT_PARALLEL_FRONT, None, None, None, None, 0
@@ -645,9 +665,9 @@ def segment_triangle_intersection(segment, triangle):
     #     r = a / b;
     r = a / b
 
-    if np.isclose(r, 0, atol=1e-13):
+    if np.isclose(r, 0, atol=ATOL):
         r = 0
-    elif np.isclose(r, 1, atol=1e-13):
+    elif np.isclose(r, 1, atol=ATOL):
         r = 1
 
     #     if (r < 0.0)                    // ray goes away from triangle
@@ -688,9 +708,9 @@ def segment_triangle_intersection(segment, triangle):
     #     float s, t;
     #     s = (uv * wv - vv * wu) / D;
     s = (uv * wv - vv * wu) / d
-    if np.isclose(s, 0, atol=1e-14):
+    if np.isclose(s, 0, atol=ATOL):
         s = 0
-    elif np.isclose(s, 1, atol=1e-14):
+    elif np.isclose(s, 1, atol=ATOL):
         s = 1
 
     #     if (s < 0.0 || s > 1.0)         // I is outside T
@@ -704,14 +724,15 @@ def segment_triangle_intersection(segment, triangle):
     #     t = (uv * wu - uu * wv) / D;
     t = (uv * wu - uu * wv) / d
 
-    if np.isclose(t, 0, atol=1e-14):
+    if np.isclose(t, 0, atol=ATOL):
         t = 0
-    elif np.isclose(t, 1, atol=1e-14):
+    elif np.isclose(t, 1, atol=ATOL):
         t = 1
 
     st = s + t
-    if np.isclose(st, 1, atol=1e-14):
+    if np.isclose(st, 1, atol=ATOL):
         st = 1
+        t = 1 - s  # ensure consistency
 
     #     if (t < 0.0 || (s + t) > 1.0)  // I is outside T
     #         return 0;
@@ -721,17 +742,19 @@ def segment_triangle_intersection(segment, triangle):
         else:
             return NO_INT_OUTSIDE_FACE, r, s, t, intersection, b
 
+    new_intersection = triangle[0] + s * u + t * v
+
     if (s == 0 and t == 0) or (s == 1 and t == 0) or (s == 0 and t == 1):
         if swap_st:
-            return INT_VERTEX, r, t, s, intersection, b
+            return INT_VERTEX, r, t, s, new_intersection, b
         else:
-            return INT_VERTEX, r, s, t, intersection, b
+            return INT_VERTEX, r, s, t, new_intersection, b
 
     if s == 0 or t == 0 or st == 1:
         if swap_st:
-            return INT_EDGE, r, t, s, intersection, b
+            return INT_EDGE, r, t, s, new_intersection, b
         else:
-            return INT_EDGE, r, s, t, intersection, b
+            return INT_EDGE, r, s, t, new_intersection, b
 
     #     return 1;                       // I is in T
     if swap_st:
@@ -806,7 +829,7 @@ def mask_segment(segment: np.ndarray, faces: np.ndarray) -> np.ndarray:
                 ]
             )
             overlap = triangles_overlap_segment_2d(
-                subfaces, np.array(test_seg), accept_vertex_only=False
+                subfaces, test_seg, accept_vertex_only=False
             )
             if np.any(overlap):
                 mask.append(subfaces[np.argmax(overlap)])
@@ -823,7 +846,7 @@ def mask_segment(segment: np.ndarray, faces: np.ndarray) -> np.ndarray:
             elif b < 0:
                 test_seg = np.array([itrsct, segment[1]])
 
-            if np.linalg.norm(test_seg[1] - test_seg[0]) > 1e-14:
+            if ~np.isclose(np.linalg.norm(test_seg[1] - test_seg[0]), 0, atol=ATOL, rtol=RTOL):
                 if t == 0:
                     subfaces = np.array(
                         [(face[0], face[1], itrsct), (itrsct, face[1], face[2])]
@@ -839,8 +862,8 @@ def mask_segment(segment: np.ndarray, faces: np.ndarray) -> np.ndarray:
                 overlap = triangles_overlap_segment_2d(
                     subfaces, test_seg, accept_vertex_only=False
                 )
-                if np.any(overlap):
-                    mask.append(subfaces[np.argmax(overlap)])
+
+                mask.extend(subfaces[overlap])
         elif res == INT_VERTEX:
             # in that case we add the face to the mask if the rear half-segment 2D intersects
             # with the face
@@ -849,7 +872,7 @@ def mask_segment(segment: np.ndarray, faces: np.ndarray) -> np.ndarray:
             elif b < 0:
                 test_seg = np.array([itrsct, segment[1]])
 
-            if np.linalg.norm(test_seg[1] - test_seg[0]) > 1e-14:
+            if ~np.isclose(np.linalg.norm(test_seg[1] - test_seg[0]), 0, atol=ATOL, rtol=RTOL):
                 overlap = triangles_overlap_segment_2d(
                     np.array([face]), test_seg, accept_vertex_only=False
                 )
@@ -868,7 +891,7 @@ def mask_segment(segment: np.ndarray, faces: np.ndarray) -> np.ndarray:
     # TODO: cases where we might want to keep a point
     # - seg parallel to camera axis
     # - others?
-    if np.isclose(msk_seg.length, 0, atol=1e-14):
+    if np.isclose(msk_seg.length, 0, atol=ATOL, rtol=RTOL):
         # segments with identical start/stop location are sometime returned
         return np.empty(shape=(0, 2, 3))
     elif msk_seg.geom_type == "LineString":

lines/rendered_scene.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Union, Sequence
+from typing import Union, Sequence, Set
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -78,18 +78,22 @@ class RenderedScene:
         black: bool = True,
         show_axes: bool = False,
         show_grid: bool = False,
-        show_hidden: bool = False,
+        show_hidden: Union[int, Sequence[int], bool] = False,
         show_faces: Union[int, Sequence[int], bool] = False,
+        highlighted_segments: Union[int, Sequence[int], None] = None,
     ) -> None:
         """
         Display the rendered scene with matplotlib.
         :param black: if True (default), the scene is rendered in black, otherwise let
-                        matplotlib assign a per-segment colour
+            matplotlib assign a per-segment colour
         :param show_axes: if True, axes are displayed
         :param show_grid: if True, grid are displayed
         :param show_hidden: if True, hidden segment are displayed with dotted lines
         :param show_faces: if True, faces are displayed in transparent green, alternatively you
             may pass one or more int to specify which face to plot
+        :param highlighted_segments: one or more index of segment(s) to highlight (note that
+            ``merge_lines()`` may drastically affect the length and order of segment list as
+            those are merged into line strings).
         """
 
         if show_faces is not False:
@@ -103,11 +107,30 @@ class RenderedScene:
             for face in self._projected_faces[indices, :, :]:
                 plt.plot(face[[0, 1, 2, 0], 0], face[[0, 1, 2, 0], 1], "g-", lw=0.5, alpha=0.5)
                 plt.fill(face[[0, 1, 2, 0], 0], face[[0, 1, 2, 0], 1], "g", alpha=0.2)
-        if show_hidden:
-            for seg in self._projected_segments:
+        if show_hidden is not False:
+            if type(show_hidden) == int:
+                indices = [show_hidden]
+            elif type(show_hidden) == bool:
+                indices = list(range(len(self._projected_segments)))
+            else:
+                indices = show_hidden
+            for seg in self._projected_segments[indices, :, :]:
                 plt.plot(seg[:, 0], seg[:, 1], "k:", lw=0.5)
-        for ls in self.mls:
-            plt.plot(*ls.xy, "k-" if black else "-", solid_capstyle="round")
+
+        if highlighted_segments is None:
+            highlighted_segments = []
+        elif type(highlighted_segments) == int:
+            highlighted_segments = [highlighted_segments]
+        for i, ls in enumerate(self.mls):
+            kwargs = {}
+            if i in highlighted_segments:
+                kwargs = {"lw": 1.5}
+                style = "r.-"
+            elif black:
+                style = "k-"
+            else:
+                style = "-"
+            plt.plot(*ls.xy, style, solid_capstyle="round", **kwargs)
         plt.axis("equal")
         if not show_axes:
             plt.axis("off")
@@ -125,30 +148,23 @@ class RenderedScene:
         :param file_name: file name
         """
 
-        if file_name.endswith('.svg'):
+        if file_name.endswith(".svg"):
             self.save_svg(file_name, **kwargs)
         else:
             # TODO: support more format
             logging.warning(f"Unsupported file format for '{file_name}'")
 
-    def save_svg(self, file_name:str, **kwargs) -> None:
+    def save_svg(self, file_name: str, **kwargs) -> None:
         """
         TODO: hacked dimensions
         """
-        dwg = svgwrite.Drawing(
-            file_name,
-            size=(1000, 1000),
-            profile="tiny",
-            debug=False,
-        )
+        dwg = svgwrite.Drawing(file_name, size=(1000, 1000), profile="tiny", debug=False)
 
         for line in self.mls:
             dwg.add(
                 dwg.path(
                     "M"
-                    + " L".join(
-                        f"{500 * (x + 1)},{500 * (1 - y)}" for x, y in line.coords
-                    ),
+                    + " L".join(f"{500 * (x + 1)},{500 * (1 - y)}" for x, y in line.coords),
                     fill="none",
                     stroke="black",
                     **kwargs,
@@ -156,3 +172,35 @@ class RenderedScene:
             )
 
         dwg.save()
+
+    def find_indices(self) -> Set[int]:
+        """
+        **(Debug/test function)** Find the indices in the list of projected segment
+        corresponding to the final list of displayed segment. This function is mainly used
+        for debugging purposes and tests.
+        :return: set of indices
+        """
+        idx = set()
+        for ls in self.mls:
+            segment = np.array(ls)
+            segments = np.tile(
+                segment.reshape((1, 2, 2)), (len(self._projected_segments), 1, 1)
+            )
+
+            res = np.logical_or(
+                np.all(
+                    np.all(np.isclose(self._projected_segments[:, :, 0:2], segments), axis=-1),
+                    axis=-1,
+                ),
+                np.all(
+                    np.all(
+                        np.isclose(self._projected_segments[:, [1, 0], 0:2], segments), axis=-1
+                    ),
+                    axis=-1,
+                ),
+            )
+
+            if np.sum(res) == 1:
+                idx.add(int(np.argmax(res)))
+
+        return idx

tests/test_math.py
@@ -24,9 +24,13 @@ from lines.math import (
     INT_VERTEX,
     DEGENERATE,
     mask_segment,
+    ATOL,
 )
 from lines.tables import CUBE_VERTICES, CUBE_SEGMENTS, CUBE_FACES
 from tests.utils import segment_list_equal
+
+
+FACTOR_LIST = [1e6, 1e3, 1, 1e-3, 1e-6]
 
 
 def test_vertices_matmul_empty():
@@ -337,11 +341,12 @@ TRIANGLES_OVERLAP_SEGMENT_2D_TEST_DATA = [
 ]
 
 
+@pytest.mark.parametrize("factor", FACTOR_LIST)
 @pytest.mark.parametrize(("s", "expected"), TRIANGLES_OVERLAP_SEGMENT_2D_TEST_DATA)
-def test_triangles_overlap_segment_2d(s, expected):
-    triangles = np.array(list(itertools.permutations([(1, 0), (0, 1), (0, 0)])))
+def test_triangles_overlap_segment_2d(s, expected, factor):
+    triangles = factor * np.array(list(itertools.permutations([(1, 0), (0, 1), (0, 0)])))
 
-    for segment in [np.array(s), np.array([s[1], s[0]])]:
+    for segment in [factor * np.array(s), factor * np.array([s[1], s[0]])]:
         if expected is None:
             assert np.all(triangles_overlap_segment_2d(triangles, segment, True))
             assert np.all(~triangles_overlap_segment_2d(triangles, segment, False))
@@ -406,23 +411,24 @@ SEGMENT_TRIANGLE_INTERSECTION_TEST_DATA = [
 ]
 
 
+@pytest.mark.parametrize("factor", FACTOR_LIST)
 @pytest.mark.parametrize(
     ("seg", "result", "intersection", "expected_r", "expected_s", "expected_t"),
     SEGMENT_TRIANGLE_INTERSECTION_TEST_DATA,
 )
 def test_segment_triangle_intersection(
-    seg, result, intersection, expected_r, expected_s, expected_t
+    seg, result, intersection, expected_r, expected_s, expected_t, factor
 ):
     base_triangle = [(0, 0, 0), (1, 0, 0), (0, 1, 0)]
     for triangle in itertools.permutations(base_triangle):
         for segment, invert_r in [(np.array(seg), False), (np.array([seg[1], seg[0]]), True)]:
             res, r, s, t, itrsct, _ = segment_triangle_intersection(
-                segment, np.array(triangle)
+                factor * segment, factor * np.array(triangle)
             )
 
             assert res == result
             if intersection is not None:
-                assert np.all(np.isclose(itrsct, np.array(intersection), atol=1e-14))
+                assert np.all(np.isclose(itrsct, factor * np.array(intersection), atol=1e-14))
             if expected_r is not None:
                 assert np.isclose(
                     r, expected_r if not invert_r else 1 - expected_r, atol=1e-14
@@ -474,8 +480,23 @@ def test_segments_are_equal():
     )
 
 
+def check_mask_segment(segment, expected_masked_segment, triangle, factor):
+    decimals = int(np.log10(factor) + np.log10(np.linalg.norm(segment)) + np.log10(ATOL))
+
+    if expected_masked_segment is None:
+        exp_res = np.array([segment])[:, :, 0:2]
+    else:
+        exp_res = np.array(expected_masked_segment)
+    for face in itertools.permutations(triangle):
+        for s in [segment, (segment[1], segment[0])]:
+            results = mask_segment(factor * np.array(s), factor * np.array([face]))
+            assert segment_list_equal(
+                results[:, :, 0:2].round(decimals), (factor * exp_res).round(decimals)
+            )
+
+
 # if expected result is None, the test assumes it is identical to input
-MASK_SEGMENT_TEST_DATA = [
+MASK_SEGMENT_TEST_DATA_FLAT_TRIANGLE = [
     # vertex interaction
     ([(-1, -1, -1), (0, 0, 0)], None),
     ([(-1, -1, 1), (0, 0, 0)], None),
@@ -524,16 +545,33 @@ MASK_SEGMENT_TEST_DATA = [
 ]
 
 
-@pytest.mark.parametrize(("segment", "expected_masked_segment"), MASK_SEGMENT_TEST_DATA)
-def test_mask_segment(segment, expected_masked_segment):
-    if expected_masked_segment is None:
-        exp_res = np.array([segment])[:, :, 0:2]
-    else:
-        exp_res = np.array(expected_masked_segment)
-    for triangle in itertools.permutations([(0, 0, 0), (0, 1, 0), (1, 0, 0)]):
-        for s in [segment, (segment[1], segment[0])]:
-            results = mask_segment(np.array(s), np.array([triangle]))
-            assert segment_list_equal(results[:, :, 0:2], exp_res)
+@pytest.mark.parametrize("factor", FACTOR_LIST)
+@pytest.mark.parametrize(
+    ("segment", "expected_masked_segment"), MASK_SEGMENT_TEST_DATA_FLAT_TRIANGLE
+)
+def test_mask_segment_flat_triangle(segment, expected_masked_segment, factor):
+    check_mask_segment(
+        segment, expected_masked_segment, [(0, 0, 0), (0, 1, 0), (1, 0, 0)], factor
+    )
+
+
+# if expected result is None, the test assumes it is identical to input
+MASK_SEGMENT_TEST_DATA_TILTED_TRIANGLE = [
+    # Intersection outside
+    ([(-1, -1, -1), (0, 0, 0)], None),
+    ([(-1, -1, 1), (0, 0, 0)], None),
+    # TODO: to be completed!
+]
+
+
+@pytest.mark.parametrize("factor", FACTOR_LIST)
+@pytest.mark.parametrize(
+    ("segment", "expected_masked_segment"), MASK_SEGMENT_TEST_DATA_TILTED_TRIANGLE
+)
+def test_mask_segment_tilted_triangle(segment, expected_masked_segment, factor):
+    check_mask_segment(
+        segment, expected_masked_segment, [(1, 1, 0), (1, -1, 0), (0, 0, 1)], factor
+    )
 
 
 # In this test we consider only segment parallel to XY face, so the test can generate z

tests/test_scene.py
@@ -1,8 +1,6 @@
 import pytest
 
-import numpy as np
-
-from lines import Scene, SegmentShape, TriangleShape, Pyramid
+from lines import Scene, SegmentShape, TriangleShape, Pyramid, StrippedCube
 
 
 @pytest.fixture(params=["v1", "v2"])
@@ -30,36 +28,27 @@ def test_no_segment(renderer_id):
     scene.render(renderer_id)
 
 
-# FIXME: use renderer_id
-def test_pyramid():
+def test_pyramid(renderer_id):
     scene = Scene()
     scene.add(Pyramid())
     scene.look_at((2, 6, 1.5), (0, 0, 0))
     scene.perspective(70, 0.1, 10)
-    rs = scene.render("v2", merge_lines=False)
+    rs = scene.render(renderer_id, merge_lines=False)
 
-    expected_idx = {1, 2, 5, 6, 7}
+    assert rs.find_indices() == {1, 2, 5, 6, 7}
 
-    idx = set()
-    for ls in rs.mls:
-        segment = np.array(ls)
-        res = np.all(
-            np.all(
-                np.logical_or(
-                    np.isclose(
-                        rs._projected_segments[:, :, 0:2],
-                        np.tile(segment.reshape((1, 2, 2)), (8, 1, 1)),
-                    ),
-                    np.isclose(
-                        rs._projected_segments[:, [1, 0], 0:2],
-                        np.tile(segment.reshape((1, 2, 2)), (8, 1, 1)),
-                    ),
-                ),
-                axis=1,
-            ),
-            axis=1,
-        )
 
-        assert np.sum(res) == 1
-        idx.add(np.argmax(res))
-    assert idx == expected_idx
+def test_city(renderer_id):
+    """
+    This test case appeared as problematic at some point
+    """
+    for i, j in [(0, 0), (-4, -2)]:
+        scene = Scene()
+        c = StrippedCube(scale=(1, 1, 1), translate=(i, j, 0.5))
+        scene.add(c)
+        scene.look_at((1.1, 0.8, 8.2), (0, 0.2, 0))
+        scene.perspective(90, 0.1, 10)
+        rs = scene.render(renderer_id, merge_lines=False)
+
+        expected_indices = {8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
+        assert rs.find_indices() == expected_indices